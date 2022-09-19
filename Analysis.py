51376f4d11e5ecd1a3af1c2e0048e7180293dc1c
igorchem <barden.igor@gmail.com>
2022-09-19 15:53:32 -0300

    j

--- Analysis.py
+++ Analysis.py
@@ -12,41 +12,12 @@
 import os, glob, sys
 import numpy as np
 #--------------------------------------------------------------
-VISMOL_HOME = os.environ.get('VISMOL_HOME')
-HOME        = os.environ.get('HOME')
-if not VISMOL_HOME == None: sys.path.append(os.path.join(VISMOL_HOME,"easyhybrid/pDynamoMethods") ) 
-else:                       sys.path.append(os.path.join("/home/igorchem/easyhybrid/pDynamoMethods") ) 
 #-----------------------------------------------------------------------------------------------------
 #Loading own libraries
 #-------------------------------------------------------------
 from EnergyAnalysis     	import EnergyAnalysis
 from TrajectoryAnalysis 	import TrajectoryAnalysis
 #-------------------------------------------------------------
-from GeometrySearcher 	    import GeometrySearcher
-from RelaxedScan 			import SCAN
-from MolecularDynamics  	import MD
-from UmbrellaSampling  	    import US
-from PotentialOfMeanForce   import PMF
-from ReactionCoordinate 	import ReactionCoordinate
-from EnergyRefinement	 	import EnergyRefinement
-#--------------------------------------------------------------
-#loading pDynamo Libraries
-from pBabel                    import *                                     
-from pCore                     import *
-#---------------------------------------                                     
-from pMolecule                 import *                              
-from pMolecule.MMModel         import *
-from pMolecule.NBModel         import *                                     
-from pMolecule.QCModel         import *
-#---------------------------------------
-from pScientific               import *                                     
-from pScientific.Arrays        import *                                     
-from pScientific.Geometry3     import *                                     
-from pScientific.RandomNumbers import *                                     
-from pScientific.Statistics    import *
-from pScientific.Symmetry      import *
-#--------------------------------------                                    
-from pSimulation               import *
 #=======================================================================
 class Analysis:
 	'''
