5b608fa50758d879ffd9156222b3204aad341e71
bardenChem <primordia.lqqc@gmail.com>
2024-06-20 17:02:05 -0300

    orca tested

---
+++ Tests/test_02_orca.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pDynamo_Scripts import Scripts
+import SimulationSystem 
+import os
+#===================================
+def Run_Test():
+	'''
+	Test the setting of all quantum methods classes available.
+	Internal semi-empirical methods in pDynamo
+	DFTB+ 
+	Orca 
+	Mopac
+	pySCF 
+	'''
+	_parameters = {
+		"Input_Type":"geometry",
+		"crd_file":os.path.join("data","cyclohexane_single_frame.xyz"),
+		"set_energy_model":"QM",
+		"functional":"HF",
+		"method_class":"ORCA",
+		"basis":"6-31G*",
+		"scratch":"test_02_orca_cyclohex"
+	}
+	
+	test_02 = Scripts("test_02_orca_cyclohex")
+	test_02.Set_System(_parameters)	
+	test_02.SaveSystem()
+	#--------------------------------
+	#test QC/MM from gromacs
+	_parameters["Input_Type"]      ="pkl"
+	_parameters["pkl_file"]        ="test_01/1atp_peptide.pkl"
+	_parameters["set_qc_region"]   ="yes"
+	_parameters["residue_patterns"]=["*:ARG.19:*"]
+	_parameters["QCcharge"]        = 1
+	_parameters["scratch"]		   = "test_02_orca_qmmm_1atp"
+
+	test_03 = Scripts("test_02_orca_qmmm_1atp")
+	test_03.Set_System(_parameters)
+	test_03.SaveSystem()
+
+
+	#test QC/MM from AMBER
+	_parameters["residue_patterns"]= ["*:LIG.248:*","*:GLU.164:*","*:HIE.94:*"]
+	_parameters["pkl_file"]        = "test_01/7tim.pkl"
+	_parameters["scratch"]		   = "test_02_orca_qmmm_tim"
+
+	test_04 = Scripts("test_02_orca_qmmm_tim")
+	test_04.Set_System(_parameters)
+	test_04.SaveSystem()
+
+
+
+
+
+	
+#===================================
+if __name__ == '__main__': Run_Test()
---
+++ Tests/test_02_smo.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pDynamo_Scripts import Scripts
+import SimulationSystem 
+import os
+#===================================
+def Run_Test():
+	'''
+	Test the setting of all quantum methods classes available.
+	Internal semi-empirical methods in pDynamo
+	DFTB+ 
+	Orca 
+	Mopac
+	pySCF 
+	'''
+	SMOmodels = ["am1","am1dphot","pddgpm3","pm3","rm1","pm6"]		
+	_parameters = {
+		"Input_Type":"geometry",
+		"crd_file":os.path.join("data","cyclohexane_single_frame.xyz"),
+		"set_energy_model":"QM",
+		"Hamiltonian":"am1",
+		"method_class":"SMO"
+	}
+	for smo in SMOmodels:
+		_parameters["Hamiltonian"] = smo
+		test_01 = Scripts("test_02")
+		test_01.Set_System(_parameters)
+		test_01.SaveSystem("cyclohexane"+smo+".pkl")
+	
+	#test QC/MM from gromacs
+	_parameters["Input_Type"]      ="pkl"
+	_parameters["pkl_file"]        ="test_01/1atp_peptide.pkl"
+	_parameters["set_qc_region"]   ="yes"
+	_parameters["residue_patterns"]=["*:ARG.19:*"]
+	_parameters["QCcharge"]        = 1
+
+	test_02 = Scripts("test_02")
+	test_02.Set_System(_parameters)
+	test_02.SaveSystem("1atp_peptide_qmmm.pkl")
+
+	#test QC/MM from AMBER
+	_parameters["residue_patterns"] = ["*:LIG.248:*","*:GLU.164:*","*:HIE.94:*"]
+	_parameters["pkl_file"]         = "test_01/7tim.pkl"
+	test_03 = Scripts("test_02")
+	test_03.Set_System(_parameters)
+	test_03.SaveSystem("7tim_qcmm_am1.pkl")
+
+
+	#test QC/MM com AMBER carregado de pkl
+
+
+	
+#===================================
+if __name__ == '__main__': Run_Test()
