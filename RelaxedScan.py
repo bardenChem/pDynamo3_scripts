7c6b48d5def7987f146e3b191d8770be08b5ce56
bardenChem <primordia.lqqc@gmail.com>
2025-02-10 19:29:10 -0300

    k

--- RelaxedScan.py
+++ RelaxedScan.py
@@ -497,8 +497,8 @@ class SCAN:
 
         for i in range ( 1, X ):
             if self.adaptative:
-            try: self.ChangeConvergenceParameters(i-1,0) 
-            except: pass
+                try: self.ChangeConvergenceParameters(i-1,0) 
+                except: pass
             distance_1 = self.DMINIMUM[0] + self.DINCREMENT[0] * float(i)
             rmodel     = RestraintEnergyModel.Harmonic( distance_1, self.forceC[0] )
             restraint  = RestraintMultipleDistance.WithOptions(energyModel = rmodel, distances = [ [ atom2, atom1, weight1 ],[ atom2, atom3, weight2 ] ] )
