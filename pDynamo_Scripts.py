b676e8d386cc84cadf6911ee0ebee4f8818b3f84
bardenChem <primordia.lqqc@gmail.com>
2025-02-13 11:22:01 -0300

    k

--- pDynamo_Scripts.py
+++ pDynamo_Scripts.py
@@ -85,7 +85,7 @@ class Scripts:
 			for rc in range(0,_parameters["set_reaction_crd"]):
 				self.activeSystem.Set_Reaction_crd( _parameters["atoms_rc"+str(rc+1)],_parameters )
 		if "set_initial_crd" in _parameters:
-			print(_parameters["set_initial_crd"][:-4])
+			print(_parameters["set_initial_crd"][-4:])
 			input()
 			if ( _parameters["set_initial_crd"][:-4] ) == ".pkl":				
 				self.activeSystem.system.coordinates3 = Unpickle(_parameters["set_initial_crd"])[0]
